787a5dd7d1bdd9b968232175f815018c111548a4
pawl0wski <59766830+pawl0wski@users.noreply.github.com>
2022-06-14 18:57:51 +0200

    Install morgan and start server on npm run start

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,25 @@
 import express from "express";
+import morgan from "morgan";
+import DatabaseService from "./services/databaseService";
+import DatabaseConfig from "./config/databaseConfig";
+import * as dotenv from "dotenv";
 
+dotenv.config();
 
-(() => {
+(async () => {
+    const databaseConfig = new DatabaseConfig();
+    const databaseService = DatabaseService.getInstance(databaseConfig);
+    await databaseService.sync();
+    console.log("Database initialized.");
 
     const app = express();
 
-    app.listen(8080);
+    app.use(express.json());
+    app.use(morgan("short"));
+    app.set("trust proxy", true);
 
-})()
+    const PORT = process.env.PORT || 8080;
+    app.listen(PORT, () => {
+        console.log(`Server listening on http://localhost:${PORT}`);
+    });
+})();
