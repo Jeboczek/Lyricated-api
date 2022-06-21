9681e7ed702a06ef234aff7846cf256918279d3b
pawl0wski <59766830+pawl0wski@users.noreply.github.com>
2022-06-21 16:18:10 +0200

    Add getLyricById to LyricRepository

--- src/repositories/lyricRepository.ts
+++ src/repositories/lyricRepository.ts
@@ -1,3 +1,4 @@
+import LyricModel from "../models/database/api/lyricModel";
 import LyricSentenceModel from "../models/database/api/translations/lyricSentenceModel";
 import MovieModel from "../models/database/api/movieModel";
 import LangModel from "../models/database/api/langModel";
@@ -8,4 +9,11 @@ export default class LyricRepository {
         MovieModel,
         LangModel,
     ];
+
+    getLyricById(id: number): Promise<LyricModel | null> {
+        return LyricModel.findOne({
+            where: { id },
+            include: this.modelsToIncludeWithLyricModel,
+        });
+    }
 }
