406d9c8a62a1a7c57a3452412cdc4238f7d8ac04
pawl0wski <59766830+pawl0wski@users.noreply.github.com>
2022-06-25 23:03:55 +0200

    Add SearchServiceSearch

--- src/services/searchService/searchService.ts
+++ src/services/searchService/searchService.ts
@@ -1,12 +1,13 @@
-import SearchServiceFilter from "./libs/searchServiceFilter";
+import SearchServiceSearch from "./libs/searchServiceSearch";
 import SearchServiceHighlight from "./libs/searchServiceHighlight";
 import SearchRequest from "../../models/request/searchRequest";
 
 export default class SearchService {
     constructor(
-        private filter: SearchServiceFilter = new SearchServiceFilter(),
+        private filter: SearchServiceSearch = new SearchServiceSearch(),
         private highlighter: SearchServiceHighlight = new SearchServiceHighlight(),
-        private sorter: SearchServiceHighlight = new SearchServiceHighlight()
+        private sorter: SearchServiceHighlight = new SearchServiceHighlight(),
+        private searcher: SearchServiceSearch = new SearchServiceSearch()
     ) {}
 
     async search(options: SearchRequest) {
