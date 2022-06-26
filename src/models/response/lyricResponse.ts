9a1e4527889505cbe20b1427092c74322d1b9e85
pawl0wski <59766830+pawl0wski@users.noreply.github.com>
2022-06-26 17:10:39 +0200

    Send episode with LyricResponse

--- src/models/response/lyricResponse.ts
+++ src/models/response/lyricResponse.ts
@@ -1,10 +1,12 @@
 import MovieResponse from "./movieResponse";
 import LyricModel from "../database/api/lyricModel";
 import LyricSentenceResponse from "./translations/lyricSentenceResponse";
+import EpisodeResponse from "./episodeResponse";
 
 export default class LyricResponse {
     public lyric_id: number;
     public movie: MovieResponse;
+    public episode: EpisodeResponse | null;
     public minute: number;
     public quality: number | null;
     public sentences: LyricSentenceResponse[];
@@ -12,10 +14,11 @@ export default class LyricResponse {
     static fromModel(model: LyricModel) {
         const resp = new LyricResponse();
 
-        const { id, movie, minute, quality, sentences } = model;
+        const { id, movie, minute, quality, sentences, episode } = model;
 
         resp.lyric_id = id;
         resp.movie = MovieResponse.fromModel(movie);
+        resp.episode = EpisodeResponse.fromModel(episode);
         resp.quality = quality;
         resp.minute = minute;
         resp.sentences = sentences.map((e) =>
