8f5699d1112d18e28f555b8a0f3bd9c747bfd468
pawl0wski <59766830+pawl0wski@users.noreply.github.com>
2022-06-29 01:36:02 +0200

    Add method in LangRepository for deleting Lang's

--- src/repositories/langRepository/langRepository.ts
+++ src/repositories/langRepository/langRepository.ts
@@ -1,6 +1,7 @@
 import LangModel from "../../models/database/api/langModel";
 import CreateError from "../../exceptions/createError";
 import Locale from "../../locale/locale";
+import DeleteError from "../../exceptions/deleteError";
 
 export default class LangRepository {
     async createLang(lang: string): Promise<LangModel> {
@@ -9,6 +10,19 @@ export default class LangRepository {
             return await langModel.save();
         } catch (e) {
             throw new CreateError(Locale.createCreateErrorText("Lang"));
+        }
+    }
+
+    async deleteLang(lang: string): Promise<LangModel> {
+        const langModel = await LangModel.findByPk(lang);
+        if (langModel === null)
+            throw new DeleteError(Locale.createNotFoundErrorText("Lang"));
+
+        try {
+            await langModel.destroy();
+            return langModel;
+        } catch (e) {
+            throw new DeleteError(Locale.createDeleteErrorText("Lang"));
         }
     }
 }
