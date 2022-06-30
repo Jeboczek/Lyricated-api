b744b404746afff8e743335a98b29224ea5598a5
pawl0wski <59766830+pawl0wski@users.noreply.github.com>
2022-07-01 00:36:31 +0200

    Add Curse's filter

--- src/repositories/searchRepository/libs/handlers/searchRepositoryFilterHandler.ts
+++ src/repositories/searchRepository/libs/handlers/searchRepositoryFilterHandler.ts
@@ -1,14 +1,35 @@
 import SearchRepositoryAbstractHandler from "./searchRepositoryAbstractHandler";
 import SearchRepositoryState from "../../interfaces/searchRepositoryState";
 import SearchRepositoryResult from "../../interfaces/searchRepositoryResult";
+import CurseRepository from "../../../curseRepository/curseRepository";
 
 export default class SearchRepositoryFilterHandler extends SearchRepositoryAbstractHandler {
-    private _hideCurses(
+    private async _hideCurses(
         result: SearchRepositoryResult[],
         // eslint-disable-next-line @typescript-eslint/no-unused-vars
         hideCurses?: boolean
-    ): SearchRepositoryResult[] {
-        //  TODO: Implement
+    ): Promise<SearchRepositoryResult[]> {
+        if (hideCurses) {
+            const repo = new CurseRepository();
+            const curses = await repo.getCurses();
+            return result.filter((e) => {
+                const lyric = e.lyricModel;
+
+                let haveCurse = false;
+
+                // TODO: Curses should be filtered
+                for (const curse of curses) {
+                    for (const lyricSentence of lyric.sentences) {
+                        haveCurse = lyricSentence.content
+                            .toLowerCase()
+                            .includes(curse.content);
+                        if (haveCurse) return false;
+                    }
+                }
+                return true;
+            });
+        }
+
         return result;
     }
 
@@ -46,11 +67,14 @@ export default class SearchRepositoryFilterHandler extends SearchRepositoryAbstr
             hide_series: hideSeries,
         } = state.request.filter_options;
 
-        for (let result of [state.mains, state.similar]) {
+        // eslint-disable-next-line prefer-const
+        for (let [i, result] of [state.mains, state.similar].entries()) {
             result = this._onlyMovieId(result, onlyMovieId);
             result = this._hideMovies(result, hideMovies);
             result = this._hideSeries(result, hideSeries);
-            result = this._hideCurses(result, hideCurses);
+            result = await this._hideCurses(result, hideCurses);
+            if (i === 0) state.mains = result;
+            state.similar = result;
         }
 
         return await super.handle(state);
