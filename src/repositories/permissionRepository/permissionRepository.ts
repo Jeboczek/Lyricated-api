c0e64d8d7c8f5b1b4f318931a0b161ad5136b0b5
pawl0wski <59766830+pawl0wski@users.noreply.github.com>
2022-07-03 19:00:24 +0200

    Add method assignPermissionToKey in PermissionRepository

--- src/repositories/permissionRepository/permissionRepository.ts
+++ src/repositories/permissionRepository/permissionRepository.ts
@@ -3,6 +3,8 @@ import KeyModel from "../../models/database/security/keyModel";
 import CreateError from "../../exceptions/createError";
 import Locale from "../../locale/locale";
 import PermissionModel from "../../models/database/security/permissionModel";
+import UpdateError from "../../exceptions/updateError";
+import KeyToPermissionModel from "../../models/database/security/relationships/keyToPermissionModel";
 
 export default class PermissionRepository {
     async createNewKey(name: string, length = 64): Promise<KeyModel> {
@@ -37,6 +39,39 @@ export default class PermissionRepository {
             });
         } catch (e) {
             throw new CreateError(Locale.createCreateErrorText("Permission"));
+        }
+    }
+
+    async assignPermissionToKey(
+        permissionName: string,
+        key: string
+    ): Promise<KeyToPermissionModel> {
+        // Check if Permission and Key exists
+        const permissionModel = await PermissionModel.findOne({
+            where: {
+                name: permissionName,
+            },
+        });
+        if (permissionModel === null)
+            throw new UpdateError(
+                Locale.createThereIsNoObjectText("Permission")
+            );
+        const keyModel = await KeyModel.findOne({
+            where: { key },
+        });
+        if (keyModel === null)
+            throw new UpdateError(Locale.createThereIsNoObjectText("Key"));
+
+        // Create new KeyToPermissionModel
+        try {
+            return await KeyToPermissionModel.create({
+                keyId: keyModel.id,
+                permissionId: permissionModel.id,
+            });
+        } catch (e) {
+            throw new UpdateError(
+                Locale.createCreateErrorText("KeyToPermission")
+            );
         }
     }
 }
