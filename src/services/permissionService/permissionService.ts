3cde5e5b448cfc0a6d4257f4a0401f8bd3c9dd3d
pawl0wski <59766830+pawl0wski@users.noreply.github.com>
2022-07-03 21:52:23 +0200

    Implement logic for PermissionService

--- src/services/permissionService/permissionService.ts
+++ src/services/permissionService/permissionService.ts
@@ -1,3 +1,56 @@
-export default class PermissionService {
+import PermissionRepository from "../../repositories/permissionRepository/permissionRepository";
+import { Permission } from "./types/permission";
+import KeyModel from "../../models/database/security/keyModel";
 
+export default class PermissionService {
+    private static instance: PermissionService;
+    private repo: PermissionRepository;
+
+    private constructor(repo: PermissionRepository) {
+        this.repo = repo;
+    }
+
+    static getInstance(repo?: PermissionRepository) {
+        if (!PermissionService.instance) {
+            if (repo === undefined)
+                throw Error(
+                    "When you first initialize PermissionService you need to provide a PermissionRepository."
+                );
+            PermissionService.instance = new PermissionService(repo);
+        }
+        return PermissionService.instance;
+    }
+
+    async addAllDefaultPermissions() {
+        await this.repo.addPermissionIfNotExists(
+            "contributor",
+            "It allows you to modify (Create, Update, Delete) the content of the application. E.g. Movies, Languages etc."
+        );
+        await this.repo.addPermissionIfNotExists(
+            "admin",
+            "It allows you to add new keys and permissions. It also allows you to assign permissions keys."
+        );
+        await this.repo.addPermissionIfNotExists(
+            "client",
+            "It allows you to access application data. This permission should be the only one for the end user."
+        );
+    }
+
+    async createNewKey(
+        name: string,
+        permissions: Permission[]
+    ): Promise<KeyModel> {
+        const key = await this.repo.createNewKey(name);
+        for (const permission of permissions) {
+            await this.repo.assignPermissionToKey(permission, key.key);
+        }
+        return key;
+    }
+
+    async checkIfKeyHavePermission(
+        key: string,
+        permission: Permission
+    ): Promise<boolean> {
+        return await this.repo.checkIfKeyHavePermission(key, permission);
+    }
 }
---
+++ src/services/permissionService/types/permission.ts
@@ -0,0 +1,1 @@
+export type Permission = "contributor" | "admin" | "client";
